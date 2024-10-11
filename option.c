becb05db6bad90aa5fccf9084701d7d6a32c6d77
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-10-11 17:42:37 -0300

    Not open the file

--- option.c
+++ option.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
+#include <sys/time.h>
 #include <stdint.h>
 
 #include "main.h"
-#include "option.h"
 
 
 errorEvent_t touch(const char *name, flags8_t opFlags) {
-    FILE *fp = fopen(name, "r+");
-    if(fp == NULL) {
-        // The 'c' prevents the file from being created
-        if(opFlags.has_c || (fp = fopen(name, "w")) == NULL)
-            return errorHandler(ERROR_FILE, name);
-    }
-
-    fclose(fp);
 
     return 0;
 }
